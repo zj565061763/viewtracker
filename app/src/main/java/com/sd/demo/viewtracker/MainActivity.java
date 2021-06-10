3db0b3ecfbd8af5f845b5370c9d7dc6f2a5acf0d
zjun <565061763@qq.com>
2021-06-10 20:44:17 +0800

    demo

--- app/src/main/java/com/sd/demo/viewtracker/MainActivity.java
+++ app/src/main/java/com/sd/demo/viewtracker/MainActivity.java
@@ -7,27 +7,30 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.sd.lib.viewtracker.FViewTracker;
+import com.sd.demo.viewtracker.databinding.ActivityMainBinding;
 import com.sd.lib.viewtracker.ViewTracker;
 import com.sd.lib.viewtracker.ViewTracker.Position;
-import com.sd.demo.viewtracker.databinding.ActivityMainBinding;
+import com.sd.lib.viewtracker.ext.FPositionTracker;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private ActivityMainBinding mBinding;
-    private ViewTracker mViewTracker;
+    private FPositionTracker mViewTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+
+        getViewTracker().setSource(mBinding.viewSource);
+        getViewTracker().setTarget(mBinding.viewTarget);
     }
 
-    public ViewTracker getViewTracker() {
+    private FPositionTracker getViewTracker() {
         if (mViewTracker == null) {
-            mViewTracker = new FViewTracker();
+            mViewTracker = new FPositionTracker();
             // 设置回调对象
             mViewTracker.setCallback(new ViewTracker.Callback() {
                 /**
@@ -56,31 +59,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == mBinding.btnTopLeft) {
             getViewTracker().setPosition(Position.TopLeft);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnTopCenter) {
             getViewTracker().setPosition(Position.TopCenter);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnTopRight) {
             getViewTracker().setPosition(Position.TopRight);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnLeftCenter) {
             getViewTracker().setPosition(Position.LeftCenter);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnCenter) {
             getViewTracker().setPosition(Position.Center);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnRightCenter) {
             getViewTracker().setPosition(Position.RightCenter);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnBottomLeft) {
             getViewTracker().setPosition(Position.BottomLeft);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnBottomCenter) {
             getViewTracker().setPosition(Position.BottomCenter);
-            getViewTracker().update();
+            getViewTracker().start();
         } else if (v == mBinding.btnBottomRight) {
             getViewTracker().setPosition(Position.BottomRight);
-            getViewTracker().update();
+            getViewTracker().start();
         }
     }
 }
